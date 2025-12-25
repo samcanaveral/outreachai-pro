06a456b59a48428ab629de2d02218586a07680ce
samcanaveral <tam_lucas@outlook.com>
2025-12-25 01:43:44 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,10 +61,11 @@ export default function App() {
   };
 
   const handleInputChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
   };
 
   const handleSubmit = async (e) => {
